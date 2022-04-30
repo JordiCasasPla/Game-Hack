84e9ead8fe1283befc9580eb991396c1d9ebb617
jamipouchi <miquelpuigturon@gmail.com>
2022-04-30 03:27:23 +0200

    No entenc pq no va

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+// We import all the components we need in our app
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import Ranking from "./components/ranking";
+import MainGame from "./components/mainGame";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={Ranking} />
+      </Routes>
+    </Router>
   );
-}
-
-export default App;
+};
